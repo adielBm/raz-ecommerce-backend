ec45da8e68a676550562447ea1d6a9b407de19cb
adiel <adiel.bm5@gmail.com>
2021-12-26 16:55:39 +0200

    order: some additions

--- src/api/order/content-types/order/lifecycles.js
+++ src/api/order/content-types/order/lifecycles.js
@@ -1,16 +1,34 @@
-const getRandomNumber = (min = 1, max = 10) => {
-  return Math.ceil(Math.random() * (max - min + 1))
-}
+const { randomBytes } = require("crypto");
 
 module.exports = {
-  beforeCreate: (event ) => {
+  beforeCreate: (event) => {
 
-    const date = new Date()
-    const code = `${date.getFullYear()}${
-      date.getMonth() + 1
-    }${date.getDate()}-${getRandomNumber()}${getRandomNumber()}${getRandomNumber()}`
-
+    const code = randomBytes(8).toString("hex");
     event.params.data.code = code
+    
+  },
+  beforeUpdate: async (event) => {
+
+    const id = event.params.where.id
+
+    const entries = await strapi.entityService.findOne('api::order.order', id, {
+      populate: {
+        delivery: true,
+        items: {
+          populate: {
+            product: true,
+          },
+        },
+      },
+    })
+
+    const reducer = (previousValue, currentValue) => {
+      return previousValue + (currentValue.product.price * currentValue.count)
+    }
+
+    const totalItems = entries.items.reduce(reducer, 0)
+    const total = totalItems + entries.delivery.cost
+    event.params.data.total = total
 
   },
 };
--- src/index.js
+++ src/index.js
@@ -7,7 +7,133 @@ module.exports = {
    *
    * This gives you an opportunity to extend code.
    */
-  register(/*{ strapi }*/) {},
+  register({ strapi }) {
+
+    const extensionService = strapi.plugin('graphql').service('extension');
+
+    const { transformArgs } = strapi.plugin("graphql").service("builders").utils;
+
+    const { toEntityResponse } = strapi.plugin("graphql").service('format').returnTypes;
+
+
+    extensionService.shadowCRUD('api::order.order').disableQueries();
+
+
+    const extension = ({ nexus }) => ({
+      types: [
+        nexus.extendType({
+          type: "Query",
+          definition(t) {
+            t.field("orderByCode", {
+              type: "OrderEntityResponse",
+              args: { code: nexus.stringArg() },
+              async resolve(parent, args, ctx) {
+
+                console.log('resolve🔻')
+
+                const transformedArgs = transformArgs(args, {
+                  contentType: strapi.contentTypes["api::order.order"],
+                  usePagination: false
+                });
+
+                console.log('args🟠', args)
+                console.log('transformedArgs🟠', transformedArgs)
+
+                const { find } = strapi
+                  .plugin("graphql")
+                  .service("builders")
+                  .get("content-api")
+                  .buildQueriesResolvers({
+                    contentType:
+                      strapi.contentTypes["api::order.order"]
+                  });
+
+                console.log('find🟠', find)
+
+                const nodes = await find(parent, { filters: transformedArgs });
+
+                console.log('nodes🟠', nodes)
+
+
+                if (nodes.length > 0) {
+                  return {
+                    value: nodes[0]
+                  };
+                } else {
+                  throw new Error(ctx.koaContext.response.message);
+                }
+              }
+            });
+          }
+        }),
+
+        /* 💖⛔⛔⛔🈸🈚 */
+
+        nexus.extendType({
+          type: "Mutation",
+          definition(t) {
+            t.field("updateOrderComplate", {
+              type: "OrderEntityResponse",
+              args: { code: nexus.stringArg() },
+              async resolve(parent, args, context) {
+
+                const contentType = strapi.contentTypes["api::order.order"]
+
+                // get order id by code:
+                const { find } = strapi.plugin("graphql")
+                  .service("builders")
+                  .get("content-api")
+                  .buildQueriesResolvers({ contentType });
+                const nodes = await find(parent, { filters: args });
+
+                const argsToUpdate = {
+                  id: nodes[0].id,
+                  data: {
+                    status: "completed"
+                  }
+                }
+
+                const transformedArgs = transformArgs(argsToUpdate, { contentType });
+
+                // Sanitize input data
+                // const sanitizedInputData = await sanitize.contentAPI.input(
+                //   transformedArgs.data,
+                //   contentType,
+                //   { auth }
+                // );
+
+                // Object.assign(transformedArgs, { data: sanitizedInputData });
+
+                const { update } = strapi.plugin("graphql")
+                  .service("builders")
+                  .get("content-api")
+                  .buildMutationsResolvers({ contentType });
+
+
+                const value = await update(parent, transformedArgs);
+
+                return toEntityResponse(value, { args: transformedArgs, resourceUID: contentType });
+              },
+
+            });
+          }
+        }),
+
+
+
+      ],
+      resolversConfig: {
+        "Query.orderByCode": {
+          auth: false
+        },
+        "Mutation.updateOrderComplate": {
+          auth: false
+        }
+      },
+    });
+
+    extensionService.use(extension);
+  },
 
   /**
    * An asynchronous bootstrap function that runs before
@@ -16,5 +142,5 @@ module.exports = {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  bootstrap(/*{ strapi }*/) { },
 };
