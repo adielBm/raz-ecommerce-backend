47835cf38f601483aedb4d66b0dff4adee9692b0
adiel <adiel.bm5@gmail.com>
2022-01-08 20:34:45 +0200

    brand post product

--- src/api/product/controllers/product.js
+++ src/api/product/controllers/product.js
@@ -6,4 +6,19 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::product.product');
+
+module.exports = createCoreController('api::product.product', ({ strapi }) => ({
+  async findOne(ctx) {
+    const { id } = ctx.params;
+    const { query } = ctx;
+
+    const entries = await strapi.entityService.findMany('api::product.product', {
+      filters: { slug: id },
+    });
+
+    const sanitizedEntity = await this.sanitizeOutput(entries[0], ctx);
+
+    return this.transformResponse(sanitizedEntity);
+  }
+}));
+
